711d8ad6a3b5a38527880b27643baf3897bbc16e
Ravi1548 <96161226+Ravi1548@users.noreply.github.com>
2022-04-01 23:28:13 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import Header from "./Header";
+import AvatarList from "./AvatarList";
+import IndividualCharacter from "./IndividualCharacter";
+import Pagination from "./Pagination";
+import Search from "./Search";
+import Loader from "./Loading";
+import DataContext from "./DataContext";
+import { useContext } from "react";
+import "./App.css";
+import Loading from "./Loading";
+
+function App() {
+  const { loading, currentCharacters, showIndividual } = useContext(DataContext);
+  return (
+    <>
+      <Header />
+      <Search />
+      {showIndividual ? (
+        <IndividualCharacter />
+      ) : loading ? (
+        <Loading />
+      ) : (
+        <AvatarList />
+      )}
+      {currentCharacters.length < 10 ? null : !showIndividual ? <Pagination /> : null}
+    </>
+  );
+}
+
+export default App;
